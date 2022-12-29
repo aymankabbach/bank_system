77bfe6be3dac92942b983ef07bc88cb069bca042
ayman <kabbachayman95@gmail.com>
2022-12-29 20:22:09 +0100

    read and validate user's choice

--- bank.cpp
+++ bank.cpp
@@ -17,9 +17,28 @@ void create_main_menu()
     vector <string> choices={"Show Client List","Add new Client","Update Client Info","Delete Client","Find Client","Exit"};
     show_choices(choices);
 }
+int read_user_choice()
+{
+    int number;
+    cout<<"what do you want to do ?";
+    cin >> number;
+    return number;
+}
+int valid_user_choice()
+{
+    int choice=read_user_choice();
+    while (choice<1 || choice>6)
+    {
+        cout<<"error, please choose an option between 1 and 6\n";
+        choice=read_user_choice();
+    }
+    return choice;
+}
 int main()
 {
     create_main_menu();
+    int user_choice=valid_user_choice();
+    cout<<user_choice<<endl;
     system("pause");
     return 0;
 }
