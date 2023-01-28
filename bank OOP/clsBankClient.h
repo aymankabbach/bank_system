a393dc742768212e51cda5b4058e42ff2c4a09d2
ayman <kabbachayman95@gmail.com>
2023-01-28 14:49:20 +0100

    starting the OOP version project

--- bank OOP/clsBankClient.h
+++ bank OOP/clsBankClient.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <iostream>
 #include "clsPerson.h"
-
+# include "clsString.h"
 using namespace std;
 
 class clsBankClient : public clsPerson
--- bank OOP/main.cpp
+++ bank OOP/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include "clsBankClient.h"
 using namespace std;
 
 int main()
