26062b28a182a3c7d581b87ae7b7a3f9d029be17
ayman <kabbachayman95@gmail.com>
2023-01-28 15:15:03 +0100

    create class Person

--- bank OOP/clsPerson.h
+++ bank OOP/clsPerson.h
@@ -5,4 +5,26 @@ using namespace std;
 
 class clsPerson
 {
+private:
+    string _Name;
+public:
+    clsPerson(string Name)
+    {
+        _Name=Name;
+    }
+    void set_name(string Name)
+    {
+        _Name=Name;
+    }
+    string get_name()
+    {
+        return _Name;
+    }
+    void get_info()
+    {
+        cout<<"Client Info \n";
+        cout<<"\n";
+        cout<<"Name : "<<get_name();
+        cout<<endl;
+    }
 };
