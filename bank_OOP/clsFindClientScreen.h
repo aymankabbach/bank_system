da4e471f45a71641c80bf6c11c13a8b8baa089b2
ayman <kabbachayman95@gmail.com>
2023-02-01 19:08:34 +0100

    library to display client's detail

---
+++ bank_OOP/clsFindClientScreen.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <iostream>
+#include "clsScreen.h"
+#include "clsBankClient.h"
+#include <iomanip>
+
+using namespace std;
+
+class clsFindClientScreen: protected clsScreen
+{
+private:
+    static void _Print_client_record_line(clsBankClient Client)
+    {
+        cout << setw(8) << left << "" << "| " << setw(15) << left << Client.get_ID();
+        cout << "| " << setw(20) << left << Client.get_name();
+        cout << "| " << setw(12) << left << Client.get_Balance();
+    }
+public:
+    static void Show_client()
+    {
+        string ID=clsBankClient::get_ID_from_user();
+        while (clsBankClient::IsClientExist(ID)==false)
+        {
+            cout<<"client with "+ID+" ID does not exit"<<endl;
+            ID=clsBankClient::get_ID_from_user();
+        }
+        system("cls");
+        _DrawScreenHeader("Client :",ID);
+        clsBankClient client=clsBankClient::Find(ID);
+        _Print_client_record_line(client);
+    }
+};
