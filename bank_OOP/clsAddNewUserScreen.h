fcb81274472680cf4db1d1285205b3e5b73f4a03
ayman <kabbachayman95@gmail.com>
2023-02-04 22:44:11 +0100

    Permissions function added

--- bank_OOP/clsAddNewUserScreen.h
+++ bank_OOP/clsAddNewUserScreen.h
@@ -9,6 +9,62 @@ using namespace std;
 
 class clsAddNewUserScreen: protected clsScreen
 {
+private:
+    static int _Read_Permissions_To_Set()
+    {
+        int Permissions = 0;
+        char Answer = 'n';
+        cout << "\nDo you want to give full access? y/n? ";
+        cin >> Answer;
+        if (Answer == 'y' || Answer == 'Y')
+        {
+            return -1;
+        }
+        cout << "\nDo you want to give access to : \n ";
+        cout << "\nShow Client List? y/n? ";
+        cin >> Answer;
+        if (Answer == 'y' || Answer == 'Y')
+        {
+            Permissions += clsUser::enPermissions::pListClients;
+        }
+        cout << "\nAdd New Client? y/n? ";
+        cin >> Answer;
+        if (Answer == 'y' || Answer == 'Y')
+        {
+            Permissions += clsUser::enPermissions::pAddNewClient;
+        }
+        cout << "\nDelete Client? y/n? ";
+        cin >> Answer;
+        if (Answer == 'y' || Answer == 'Y')
+        {
+            Permissions += clsUser::enPermissions::pDeleteClient;
+        }
+        cout << "\nUpdate Client? y/n? ";
+        cin >> Answer;
+        if (Answer == 'y' || Answer == 'Y')
+        {
+            Permissions += clsUser::enPermissions::pUpdateClients;
+        }
+        cout << "\nFind Client? y/n? ";
+        cin >> Answer;
+        if (Answer == 'y' || Answer == 'Y')
+        {
+            Permissions += clsUser::enPermissions::pFindClient;
+        }
+        cout << "\nTransactions? y/n? ";
+        cin >> Answer;
+        if (Answer == 'y' || Answer == 'Y')
+        {
+            Permissions += clsUser::enPermissions::pTranactions;
+        }
+        cout << "\nManage Users? y/n? ";
+        cin >> Answer;
+        if (Answer == 'y' || Answer == 'Y')
+        {
+            Permissions += clsUser::enPermissions::pManageUsers;
+        }
+        return Permissions;
+    }
 public:
     static void show_AddNewUser_screen()
     {
@@ -21,6 +77,7 @@ public:
         }
         clsUser new_user=clsUser::Get_Add_New_User_Object(Username);
         new_user.get_info(new_user);
+        new_user.set_Permissions(_Read_Permissions_To_Set());
         clsUser::enSaveResults SaveResults=new_user.Save();
         switch (SaveResults)
         { 
